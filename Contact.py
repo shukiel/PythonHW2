dee8aa5a89089aae058ec2df6d446f339a234827
enoshcohen <enoshc@gmail.com>
2015-12-12 14:42:04 +0200

    ...

--- Contact.py
+++ Contact.py
@@ -6,7 +6,7 @@
 class Contact:
     name = ""
 
-    def __init__(self, olderContact = None):
+    def __init__(self, olderContact=None):
         if olderContact != None:
             self = olderContact
         else:
@@ -19,22 +19,34 @@ class Contact:
         return "Name: " + self.name + "/tPhone# :" + self.phone
 
     def readValues(self):
-
+        pass #TODO
 
     def match(self, strToMatch):
-        pass
+        pass #TODO
 
 
 class FriendContact(Contact):
-    homePhone = -1
-    personalEmail = ""
-
     def __init__(self):
         pass
 
+    '''
+    def __lt__(self, other):
+        return super().__lt__(self, other)
+    '''
+
+    def __str__(self):
+        return super().__str__() + "\tHome Phone#:" + self.homePhone + "\tPersonal E-Mail: " + self.personalEmail
+
+    def strWithoutName(self):
+        return "\tHome Phone#:" + self.homePhone + "\tE-Mail: " + self.personalEmail
+
 
 class ProfessionalContact(Contact):
-    pass
+    def __init__(self, olderContact):
+        pass
+
+    def __str__(self):
+        return super().__str__() + "\tWork Phone#:" + self.workPhone + "\tWork E-Mail: " + self.workEmail
 
 
 class ProfessionalFriendContact(FriendContact, ProfessionalContact):
